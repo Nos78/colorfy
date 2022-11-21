ab27683a57f42b4b95ad5f3a0da5db9829c79b74
Noscere <noscere1978@gmail.com>
2022-11-21 23:01:08 +0000

    Initial commit of project structure and skeleton files

---
+++ colorfy/colorfy.py
@@ -0,0 +1,6 @@
+# @Author: Noscere
+# @Email: noscere1978@gmail.com
+# @Date: 2022-11-19 23:07:28.856
+# @Last Modified by:   undefined
+# @Last Modified time: 2022-11-19 23:07:28.856
+# @Description: colorfy package
